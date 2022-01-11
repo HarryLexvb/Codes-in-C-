c1517cbab7ab91a643a7cf38e0842acd6a03aa1f
HarryLexvb <74415056+HarryLexvb@users.noreply.github.com>
2022-01-11 17:26:52 -0500

    Update

---
+++ 1. repetition of numbers in an array/Exercise.md
@@ -0,0 +1,42 @@
+## EXERCISE 
+
+Write a c ++ program that complies with the following items:
+
+- Create a dynamic array of size N.
+- Print the array.
+- Enter an integer X that allows repeating the element X times
+
+#### Example:
+
+ **Input:**
+ 
+| Meessage |  Input |   
+| ------------ | ------------ | 
+| Enter the size N of the array:  | 3  |
+| Enter the number of repetitions X: | 3  |
+
+ **Ouput:**
+
+| Ouput meessage |  Input |   
+| ------------ | ------------ | 
+| Original array: | [2 -  7  - 4 ] |
+| Enter the number of repetitions X: | [2 - 2 - 2 - 7 - 7 - 7 - 4 - 4 - 4]  |
+
+The console display will be something like this:
+
+**Input:**
+
+Enter the size N of the array: 3
+
+enter the number of repetitions X: 3
+
+**output:**
+
+Original array:
+
+[2 | 7 | 4 |]
+
+Array with repetitions:
+
+[2 | 2 | 2 | 7 | 7 | 7 | 4 | 4 | 4] 
+
---
+++ 1. repetition of numbers in an array/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+/*this program consists of generating an array, printing and repeating the element as many times as the user wants*/
+
+void generate_array(int *arr, int Size){
+  for(int i = 0; i < Size; ++i){ //llenamos el array
+    cout << "\nenter element: "; cin >> arr[i];
+    cout << endl;
+  }
+}
+
+void print(int *arr, int Size){
+    cout << "[";
+  for(int i = 0; i < Size; ++i)
+    cout << arr[i] << " | ";
+  cout << "]";
+  //delete arr;
+}
+
+ void repeated(const int *arr, int *arr2, int rep){
+    cout << "\n\narray with repeats \n\n";
+    //repetitions must be greater than or equal to the one entered
+    for(int i = 0; i < rep; ++i){
+      /*arr2[i+0] = arr[0];
+      arr2[i+3] = arr[1];
+      arr2[i+6] = arr[2]; // */
+      for(int j = 0; j < rep; ++j){
+          arr2[j+(rep * i)] = arr[i];
+      }
+    }
+ }
+
+int main()
+{
+  int *arr, Size, *arr2, mult = 0;
+
+  cout << "Enter Size: "; cin >> Size;
+
+  //reservamos memoria
+  arr = new int[Size];  //original array
+  arr2 = new int[Size*mult]; //new array
+
+    generate_array(arr, Size); //we add elements to the array
+  print(arr, Size); //print array
+
+  cout << "\n\nenter the number of times to repeat an element of the array: "; cin >> mult;
+  repeated(arr, arr2, mult); //generate repeats
+  print(arr2, Size*mult); //print the new array
+
+  delete arr;
+  delete arr2;
+}
