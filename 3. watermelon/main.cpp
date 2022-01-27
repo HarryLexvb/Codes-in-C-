1b8eddd90c9b41827e648f339348c4ed76fecf00
HarryLexvb <harold.villanueva@ucsp.edu.pe>
2022-01-27 11:30:34 -0500

    update

--- 3. watermelon/main.cpp
+++ 3. watermelon/main.cpp
@@ -6,9 +6,14 @@ using namespace std;
 int main() {
 
     int w;
-    cout << "enter a number: "; cin >> w;
+    cin >> w;
+    
     bitset<1> numero(w);
-    if(numero == 0) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    
+    if(w != 2){
+        if(numero == 0 ) cout << "Yes" << endl;
+        else  cout << "No" << endl;
+    }
+    if(w == 2) cout << "No" << endl;
 
 }
